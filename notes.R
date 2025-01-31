2315820995585bd0a6cb8773cd33d0869a08643f
Scott Schumacker <scottschu97@gmail.com>
2025-01-30 21:22:31 -0600

    Created visualization

--- notes.R
+++ notes.R
@@ -1,2 +1,16 @@
 # Scott Schumacker
 # notes
+
+# Loading libraries
+library(ggplot2)
+library(dplyr)
+
+# Preview data
+head(women)
+
+# Creating plot to visualize relationship
+women %>% 
+  ggplot(aes(x = height, y = weight)) +
+  geom_point(size = 3, alpha = 0.8) +
+  theme_classic()
+
